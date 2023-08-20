c5e2aca6e0a8b9f1bc1be83badd36b57d8fb7483
Eduardo Alves <eduardo.lves@outlook.com>
2023-08-20 12:12:06 -0300

    Exercicio resolvido

--- ExercicioResolvidoSobreConjuntosSection15/Program.cs
+++ ExercicioResolvidoSobreConjuntosSection15/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Entities;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total user: " + set.Count());
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
